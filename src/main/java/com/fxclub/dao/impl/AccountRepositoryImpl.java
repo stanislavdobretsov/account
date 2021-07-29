45ceb4de0d29f74bda8b4a31677b2070d1609d5a
Stanislav Dobretsov <thestasoundor@gmail.com>
2021-07-29 13:25:29 +0700

    fix concurrency

--- src/main/java/com/fxclub/dao/impl/AccountRepositoryImpl.java
+++ src/main/java/com/fxclub/dao/impl/AccountRepositoryImpl.java
@@ -5,7 +5,6 @@ import com.fxclub.domain.Account;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -22,15 +21,7 @@ public class AccountRepositoryImpl implements AccountRepository {
     public Optional<Account> findById(Integer id) {
         Account account = null;
         try {
-            account = jdbcTemplate.queryForObject("SELECT id, balance FROM account WHERE id = ?", new RowMapper<Account>() {
-                @Override
-                public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    Account rsAccount = new Account();
-                    rsAccount.setId(rs.getInt("id"));
-                    rsAccount.setBalance(rs.getBigDecimal("balance"));
-                    return rsAccount;
-                }
-            }, id);
+            account = jdbcTemplate.queryForObject("SELECT id, balance FROM account WHERE id = ?", this::mapAccount, id);
         } catch (DataAccessException ignored) {
         }
 
@@ -41,15 +32,7 @@ public class AccountRepositoryImpl implements AccountRepository {
     public Optional<Account> findByIdForManipulation(Integer id) {
         Account account = null;
         try {
-            account = jdbcTemplate.queryForObject("SELECT id, balance FROM account WHERE id = ? FOR UPDATE", new RowMapper<Account>() {
-                @Override
-                public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    Account rsAccount = new Account();
-                    rsAccount.setId(rs.getInt("id"));
-                    rsAccount.setBalance(rs.getBigDecimal("balance"));
-                    return rsAccount;
-                }
-            }, id);
+            account = jdbcTemplate.queryForObject("SELECT id, balance FROM account WHERE id = ? FOR UPDATE", this::mapAccount, id);
         } catch (DataAccessException ignored) {
         }
 
@@ -66,5 +49,12 @@ public class AccountRepositoryImpl implements AccountRepository {
                 jdbcTemplate.queryForObject(sql, Integer.class, account.getBalance());
 
         return new Account(id, account.getBalance());
+    }
+
+    private Account mapAccount(ResultSet rs, int rowNum) throws SQLException {
+        Account rsAccount = new Account();
+        rsAccount.setId(rs.getInt("id"));
+        rsAccount.setBalance(rs.getBigDecimal("balance"));
+        return rsAccount;
     }
 }
